b3613df4d2db2ccff17ebb5be5813e5fcff9ed04
Kim <kim@bstro.com>
2019-11-15 16:03:21 -0800

    main nav dev

--- src/js/main.js
+++ src/js/main.js
@@ -2,28 +2,6 @@
 
 (function($) {
 
-$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
-	
-	var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-	
-	if (windowWidth < 768) {
-		if (!$(this).next().hasClass('show')) {
-			$(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
-		}
-		var $subMenu = $(this).next('.dropdown-menu');
-		$subMenu.toggleClass('show');
-	  
-	  
-		$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
-		  $('.dropdown-submenu .show').removeClass('show');
-		});
-	  
-	  
-		return false;
-	}
-  
-});
-
 // set fixed height on tabs
 function tabs_fixed_height() {
 	$('.tab-content').each(function(){
