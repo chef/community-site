aafe684d8a22b4b63c484b67ee4f638b8333d600
Shaun Yap <shaunyap@gmail.com>
2019-12-10 23:07:32 -0800

    cleaned svg sass, added inelegant js
    Signed-off-by: Shaun Yap <shaunyap@gmail.com>

--- src/js/main.js
+++ src/js/main.js
@@ -37,8 +37,30 @@
 
 	// code that makes the product grid activate!
 	$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-		console.log(e.currentTarget.hash);
-	  })
+		$(".stack-background").css("stroke", "#3c3c3c");
+		$(".stack-link").css("stroke", "#3c3c3c");
 
+		if (e.currentTarget.hash === "#infra") {
+			$(".automate-stroke").css("stroke", "#3c3c3c");
+			$(".inspec-stroke").css("stroke", "#3c3c3c");
+			$(".habitat-stroke").css("stroke", "#3c3c3c");			
+			$(".infra-stroke").css("stroke", "#DA1884");			
+		} else if (e.currentTarget.hash === "#automate") {
+			$(".automate-stroke").css("stroke", "#00aeef");
+			$(".inspec-stroke").css("stroke", "#3c3c3c");
+			$(".habitat-stroke").css("stroke", "#3c3c3c");			
+			$(".infra-stroke").css("stroke", "#3c3c3c");			
+		} else if (e.currentTarget.hash === "#inspec") {
+			$(".automate-stroke").css("stroke", "#3c3c3c");
+			$(".inspec-stroke").css("stroke", "#9957a3");
+			$(".habitat-stroke").css("stroke", "#3c3c3c");			
+			$(".infra-stroke").css("stroke", "#3c3c3c");			
+		} else if (e.currentTarget.hash === "#habitat") {
+			$(".automate-stroke").css("stroke", "#3c3c3c");
+			$(".inspec-stroke").css("stroke", "#3c3c3c");
+			$(".habitat-stroke").css("stroke", "#1aaf94");			
+			$(".infra-stroke").css("stroke", "#3c3c3c");			
+		};
+	  })
 
 } (jQuery));
